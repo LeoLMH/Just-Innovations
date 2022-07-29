221380f92650c43a6322ff9424d977ff4040e606
jupiter@pro16 <zcsjupiter@126.com>
2022-07-29 16:06:51 -0400

    Add audio to text function. Add demo of using speech2text api in main.py. Reduce size of sample audio.

--- flask/api/audio/main.py
+++ flask/api/audio/main.py
@@ -1,7 +1,8 @@
 import audio
 import os
 
-test_audio = os.path.join("sample_audio", "ted_talk.mp3")  #"shot.mp3"#
-data = audio.load_audio_file(test_audio, reset = False)
-print(audio.get_volume(data))
-print(audio.get_wordspersecond(data))
+test_audio = os.path.join("sample_audio", "ted_talk.wav")  #"shot.mp3"#
+#data = audio.load_audio_file(test_audio, reset = False)
+#print(audio.get_volume(data))
+#print(audio.get_wordspersecond(data))
+audio.audio_to_text(test_audio)
